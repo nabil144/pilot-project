6a94514ed41136965ecf6df7fdf7d6f0101674fb
Mira Hussein <mira.hussein@montyholding.com>
2025-01-29 13:23:01 +0200

    using primary annotation

--- springbootapp/src/main/java/com/example/springbootapp/common/CricketCoach.java
+++ springbootapp/src/main/java/com/example/springbootapp/common/CricketCoach.java
@@ -1,9 +1,22 @@
 package com.example.springbootapp.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CricketCoach implements Coach {
+
+    //define init method
+    @PostConstruct
+    public void doMyStartupStuff(){
+        System.out.println("in doMyStartupStuff(): " +getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    public void doMyCleanupStuff(){
+        System.out.println("in doMyCleanupStuff(): " +getClass().getSimpleName());
+    }
 
     @Override
     public String getDailyWorkout() {
---
+++ springbootapp/src/main/java/com/example/springbootapp/common/SwimCoach.java
@@ -0,0 +1,9 @@
+package com.example.springbootapp.common;
+
+public class SwimCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout() {
+        return "";
+    }
+}
