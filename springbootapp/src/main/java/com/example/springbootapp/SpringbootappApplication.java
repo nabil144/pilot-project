8de6be32755413d897dd052639c7df383cd4bc87
Mira Hussein <mira.hussein@montyholding.com>
2025-01-30 13:43:15 +0200

    database read operation

--- springbootapp/src/main/java/com/example/springbootapp/SpringbootappApplication.java
+++ springbootapp/src/main/java/com/example/springbootapp/SpringbootappApplication.java
@@ -17,8 +17,43 @@ public class SpringbootappApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
 		return runner -> {
-			createStudent(studentDAO);
+			//createMultipleStudents(studentDAO);
+
+			readStudent(studentDAO);
 		};
+	}
+
+	private void readStudent(StudentDAO studentDAO) {
+		//create student object
+		System.out.println("creating new student object");
+		Student tempStudent = new Student("mickey","mouse","clubhouse@gmail.com");
+
+		//save student
+		System.out.println("saving student obj");
+		studentDAO.save(tempStudent);
+
+		//display id of saved student
+		int theId = tempStudent.getId();
+		System.out.println("saved student, generated id: "+theId);
+
+		//retrieve based on id
+		System.out.println("retrieving student with id: "+theId);
+		Student myStudent = studentDAO.findById(theId);
+
+		//display student
+		System.out.println("found the student: "+myStudent);
+	}
+
+	private void createMultipleStudents(StudentDAO studentDAO){
+		System.out.println("creating 3 student objects...");
+		Student tempStudent1 = new Student("john","doe","john@gmail.com");
+		Student tempStudent2 = new Student("mary","public","mary@gmail.com");
+		Student tempStudent3 = new Student("bonita","applebum","paul@gmail.com");
+
+		System.out.println("saving 3 students...");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
 	}
 
 	private void createStudent(StudentDAO studentDAO) {
@@ -33,7 +68,6 @@ public class SpringbootappApplication {
 
 		//display id
 		System.out.println("saved student. generated id: "+tempStudent.getId());
-
 
 	}
 
