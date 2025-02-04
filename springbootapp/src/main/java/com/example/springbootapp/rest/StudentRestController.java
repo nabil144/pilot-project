6cb8f32d090e1d6e931e3db8e54a3768b7c9842f
Mira Hussein <mira.hussein@montyholding.com>
2025-02-04 13:16:51 +0200

    exception handling, controller advice

--- springbootapp/src/main/java/com/example/springbootapp/rest/StudentRestController.java
+++ springbootapp/src/main/java/com/example/springbootapp/rest/StudentRestController.java
@@ -4,10 +4,9 @@ import com.example.springbootapp.dao.StudentDAO;
 import com.example.springbootapp.entity.Student;
 import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,6 +35,10 @@ public class StudentRestController {
 
     @GetMapping("/students/{studentId}")
     public Student getStudent(@PathVariable int studentId){
+        //check studentId against listsize
+        if(studentId >= theStudents.size() || studentId < 0){
+            throw new StudentNotFoundException("student id not found - "+studentId);
+        }
         return theStudents.get(studentId); //just index in list, not actual id
     }
 
