f54dd1e432a3ef999c2a3fdfe851c82093ee5ff4
Mira Hussein <mira.hussein@montyholding.com>
2025-02-06 11:18:38 +0200

    student model, user selecting from form and displaying

---
+++ thymeleafdemo/src/main/java/com/example/springboot/thymeleafdemo/controller/StudentController.java
@@ -0,0 +1,42 @@
+package com.example.springboot.thymeleafdemo.controller;
+
+import com.example.springboot.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
+
+    @Value("${languages}")
+    private List<String> languages;
+
+    @GetMapping("/showStudentForm")
+    public String showForm(Model theModel){
+        //create student obj
+        Student theStudent = new Student();
+        //add student to model
+        theModel.addAttribute("student",theStudent);
+        theModel.addAttribute("countries",countries);
+        theModel.addAttribute("languages",languages);
+        return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student theStudent){
+        //log input data
+        System.out.printf("theStudent: "+theStudent.getFirstName()+" "+theStudent.getLastName());
+
+        return "student-confirmation";
+    }
+
+
+}
