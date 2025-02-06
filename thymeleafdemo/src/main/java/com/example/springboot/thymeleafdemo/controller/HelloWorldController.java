e0f291f18996ec02271709d2fe121519a1570f18
Mira Hussein <mira.hussein@montyholding.com>
2025-02-06 09:57:35 +0200

    request params, get mapping, post mapping

--- thymeleafdemo/src/main/java/com/example/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ thymeleafdemo/src/main/java/com/example/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -3,13 +3,16 @@ package com.example.springboot.thymeleafdemo.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
 
     //controller method to show initial html form
-    @RequestMapping("/showForm")
+    @GetMapping("/showForm")
     public String showForm(){
         return "helloworld-form";
     }
@@ -28,6 +31,18 @@ public class HelloWorldController {
         theName = theName.toUpperCase();
         //crate msg
         String result = "HELLO "+theName;
+        //add msg to model
+        model.addAttribute("message",result);
+        return "helloworld";
+    }
+
+    @PostMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName,
+                                          Model model){
+        //convert to all caps
+        theName = theName.toUpperCase();
+        //crate msg
+        String result = "hello from v3 "+theName;
         //add msg to model
         model.addAttribute("message",result);
         return "helloworld";
