a62484a5bb6d67eb6e73158c961c4b5a2f2c59f2
Mira Hussein <mira.hussein@montyholding.com>
2025-02-11 10:23:54 +0200

    update, delete, orderbylastname

--- spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafcrud/controller/EmployeeController.java
+++ spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafcrud/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import com.luv2code.springboot.thymeleafcrud.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -41,11 +38,29 @@ public class EmployeeController {
         return "employees/employee-form";
     }
 
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel){
+        //get employee from service/db
+        Employee theEmployee = employeeService.findById(theId);
+        //set employee in model
+        theModel.addAttribute("employee", theEmployee);
+        //send to form
+        return "employees/employee-form";
+    }
+
     @PostMapping("/save")
     public String saveEmployee(@ModelAttribute("employee") Employee theEmployee){
         //save the employee
         employeeService.save(theEmployee);
         //redirect to prevent dupe submission
+        return "redirect:/employees/list";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("employeeId") int theId){
+        //delete employee
+        employeeService.deleteById(theId);
+        //redirect to list
         return "redirect:/employees/list";
     }
 
--- spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafcrud/dao/EmployeeRepository.java
+++ spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafcrud/dao/EmployeeRepository.java
@@ -3,8 +3,11 @@ package com.luv2code.springboot.thymeleafcrud.dao;
 import com.luv2code.springboot.thymeleafcrud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
-    // that's it ... no need to write any code LOL!
+    //sort by last name
+    public List<Employee> findAllByOrderByLastNameAsc();
 
 }
