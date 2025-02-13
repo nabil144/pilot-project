bfbcb6194459c0b9cddc1766573da1d14eb49568
Mira Hussein <mira.hussein@montyholding.com>
2025-02-13 16:25:02 +0200

    find byid

--- crud-mappings-demo/src/main/java/com/example/crud_mappings_demo/CrudMappingsDemoApplication.java
+++ crud-mappings-demo/src/main/java/com/example/crud_mappings_demo/CrudMappingsDemoApplication.java
@@ -18,8 +18,18 @@ public class CrudMappingsDemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(AppDAO appDAO){
 		return runner -> {
-			createInstructor(appDAO);
+			//createInstructor(appDAO);
+			findInstructor(appDAO);
 		};
+	}
+
+	private void findInstructor(AppDAO appDAO) {
+		int theId = 2;
+		System.out.println("finding instructor with id "+ theId);
+		Instructor tempInstructor = appDAO.findInstructorById(theId);
+		System.out.println("temp instructor"+tempInstructor);
+		System.out.println("the associate instructor detail only "+tempInstructor.getInstructorDetail());
+
 	}
 
 	private void createInstructor(AppDAO appDAO) {
