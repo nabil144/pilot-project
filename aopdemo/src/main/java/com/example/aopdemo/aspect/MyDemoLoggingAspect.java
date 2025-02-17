e7f98c01e86061ce420d3d3ef75efad4c6059b29
Mira Hussein <mira.hussein@montyholding.com>
2025-02-17 15:02:43 +0200

    joinpoints

--- aopdemo/src/main/java/com/example/aopdemo/aspect/MyDemoLoggingAspect.java
+++ aopdemo/src/main/java/com/example/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,7 +1,10 @@
 package com.example.aopdemo.aspect;
 
+import com.example.aopdemo.Account;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -12,8 +15,22 @@ public class MyDemoLoggingAspect {
 
     //@Before("execution(public void add*())")
     @Before("com.example.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
-    public void beforeAddAccountAdvice(){
+    public void beforeAddAccountAdvice(JoinPoint theJoinPoint){
         System.out.println("\n=====>>> executing @Before advice on addAccount()");
+
+        //display method signature
+        MethodSignature methodSignature = (MethodSignature) theJoinPoint.getSignature();
+        System.out.println("method: "+ methodSignature);
+        //display method args
+        Object[] args = theJoinPoint.getArgs();
+        for(Object o : args){
+            System.out.println(o);
+            if(o instanceof Account){
+                Account theAccount = (Account)o;
+                System.out.println("account name "+theAccount.getName());
+                System.out.println("account leve "+theAccount.getLevel());
+            }
+        }
     }
 
 }
