982387c3054806041235fd40e6e95571f8cf623f
Mira Hussein <mira.hussein@montyholding.com>
2025-02-17 15:37:34 +0200

    @afterreturning

--- aopdemo/src/main/java/com/example/aopdemo/aspect/MyDemoLoggingAspect.java
+++ aopdemo/src/main/java/com/example/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,16 +2,32 @@ package com.example.aopdemo.aspect;
 
 import com.example.aopdemo.Account;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Aspect
 @Component
 @Order(2)
 public class MyDemoLoggingAspect {
+
+    //add new advice for @AfterReturning on the findAccounts method
+    @AfterReturning(
+            pointcut = "execution(* com.example.aopdemo.dao.AccountDAO.findAccounts(..))",
+            returning = "result"
+    )
+    public void afterReturningFindAccountsAdvice(JoinPoint thJoinPoint, List<Account> result){
+        //print method
+        String method = thJoinPoint.getSignature().toShortString();
+        System.out.println("\n======>> executing @AfterReturning on method "+method);
+        //print results of method call
+        System.out.println("\n======>> result is "+ result);
+    }
 
     //@Before("execution(public void add*())")
     @Before("com.example.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
--- aopdemo/src/main/java/com/example/aopdemo/dao/AccountDAOImpl.java
+++ aopdemo/src/main/java/com/example/aopdemo/dao/AccountDAOImpl.java
@@ -3,12 +3,27 @@ package com.example.aopdemo.dao;
 import com.example.aopdemo.Account;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
 public class AccountDAOImpl implements AccountDAO{
 
     private String name;
 
     private String serviceCode;
+
+    @Override
+    public List<Account> findAccounts() {
+        List<Account> myAccounts = new ArrayList<>();
+        Account temp1 = new Account("john","silver");
+        Account temp2 = new Account("madhu","platinum");
+        Account temp3 = new Account("luca","gold");
+        myAccounts.add(temp1);
+        myAccounts.add(temp2);
+        myAccounts.add(temp3);
+        return myAccounts;
+    }
 
     @Override
     public void addAccount(Account account, boolean vipFlag) {
