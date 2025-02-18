09df4fa43198bdf42b993b34b557ab4adea42da2
Mira Hussein <mira.hussein@montyholding.com>
2025-02-18 09:43:52 +0200

    logging, @before advice

---
+++ spring-boot-mvc-crud-demo-employees/src/main/java/com/luv2code/springboot/thymeleafdemo/aspect/DemoLoggingAspect.java
@@ -0,0 +1,42 @@
+package com.luv2code.springboot.thymeleafdemo.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+
+@Aspect
+@Component
+public class DemoLoggingAspect {
+
+    //logger
+    private Logger myLogger = Logger.getLogger(getClass().getName());
+
+    //pointcut declarations
+    @Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.controller.*.*(..))")
+    private void forControllerPackage(){}
+
+    @Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.service.*.*(..))")
+    private void forServicePackage(){}
+
+    @Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.dao.*.*(..))")
+    private void forDaoPackage(){}
+
+    @Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
+    private void forAppFlow(){}
+
+    @Before("forAppFlow()")
+    public void before(JoinPoint theJoinPoint){
+        //display method
+        String theMethod = theJoinPoint.getSignature().toShortString();
+        myLogger.info("====>> in @Before: calling method: "+ theMethod);
+        //display method arguments
+        Object[] args = theJoinPoint.getArgs();
+        for(Object tempArg : args){
+            myLogger.info("====>> argument: "+tempArg);
+        }
+    }
+}
