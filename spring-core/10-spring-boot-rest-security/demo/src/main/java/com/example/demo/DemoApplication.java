3b8bc6439f2bd1cafa3bdfc28cc1b33f210cd5be
Borhane Al Hinnawi <borhane.alhinnawi@montyholding.com>
2025-02-21 09:55:15 +0200

    hello

--- spring-core/10-spring-boot-rest-security/demo/src/main/java/com/example/demo/DemoApplication.java
+++ spring-core/10-spring-boot-rest-security/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.luv2code.springboot.cruddemo.entity;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CruddemoApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CruddemoApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
