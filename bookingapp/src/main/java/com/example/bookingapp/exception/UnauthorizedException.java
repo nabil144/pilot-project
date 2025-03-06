eb5af85e3d2c7b38a28c4746f7865b6dbb5c8e46
Mira Hussein <mira.hussein@montyholding.com>
2025-03-06 10:53:41 +0200

    removing old code

--- bookingapp/src/main/java/com/example/bookingapp/exception/UnauthorizedException.java
+++
@@ -1,7 +0,0 @@
-package com.example.bookingapp.exception;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
